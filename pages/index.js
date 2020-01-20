d76986f5ea968797235769640cba1585c72d1bed
douglas <douglassantoselias@gmail.com>
2020-01-20 13:39:48 -0300

    Add user mutation

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,59 @@
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Link from 'next/link'
+import useSWR, { mutate } from 'swr'
 
-export default () => (
-  <>
-    <Form>
-      <Form.Group controlId="username">
-        <Form.Label>Username</Form.Label>
-        <Form.Control autoComplete="username" />
-      </Form.Group>
+import { fetcher } from '../lib/fetcher'
 
-      <Form.Group controlId="password">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" autoComplete="current-password" />
-      </Form.Group>
+const createUser = `
+mutation {
+  createAppUser(data: {username: "test", password: "test"}) {
+    id
+  }
+}`
 
-      <Button variant="primary" type="submit">
-        {'Login'}
-      </Button>
-    </Form>
+const submit = (event) => {
+  event.preventDefault()
 
-    <br />
+  const { username, password } = event.target
 
-    <Link href="/register">
-      <a>Create Account</a>
-    </Link>
+  console.log(username.value)
+  console.log(password.value)
 
-    <style jsx global>{`
+  mutate('/api/user', { ...data, name: newName })
+}
+
+const Index = () => {
+  // const { data, error } = useSWR(query, fetcher)
+
+  // if (error) return <Error description="Could not fetch" />
+  // if (!data) return <Loading />
+
+  return (
+    <>
+      <Form onSubmit={submit}>
+        <Form.Group controlId="username">
+          <Form.Label>Username</Form.Label>
+          <Form.Control autoComplete="username" />
+        </Form.Group>
+
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" autoComplete="current-password" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          {'Login'}
+        </Button>
+      </Form>
+
+      <br />
+
+      <Link href="/register">
+        <a>Create Account</a>
+      </Link>
+
+      <style jsx global>{`
       body {
         margin: 0 auto;
         max-width: 30em;
@@ -34,6 +61,9 @@ export default () => (
         padding: 0 15px;
       }
       `}
-    </style>
-  </>
-)
+      </style>
+    </>
+  )
+}
+
+export default Index
