727d4e35fda42e06b4d97741cb299b7ca440ede7
douglas <douglassantoselias@gmail.com>
2020-01-20 17:26:41 -0300

    Add global style

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,16 @@ export default class MyApp extends App {
           <title>Stack Overflow Clone</title>
         </Head>
         <Component {...pageProps} />
+        <style jsx global>
+          {`
+            body {
+              margin: 0 auto;
+              max-width: 30em;
+              margin-top: 20px;
+              padding: 0 15px;
+            }
+          `}
+        </style>
       </>
     )
   }
